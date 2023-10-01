a2e91e91fa387b2038de12de446a37d7f6206e2c
Amaya De Silva <111060924+AmayaDS@users.noreply.github.com>
2023-10-01 16:20:35 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -24,3 +24,14 @@ class RightAngledTrianglePattern {
         }
     }
 }
+
+class InvertedRightAngledTrianglePattern {
+    public static void printPattern(int rows) {
+        for (int i = rows; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
